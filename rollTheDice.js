b2b617ed0b8cabdaedd6cdb9c4f2348d5539f452
Zoran Vasic <zvasic17@raf.rs>
2018-06-23 23:17:07 +0200

    deleted secondDie
    Explaining to me If statements hehe

--- rollTheDice.js
+++ rollTheDice.js
@@ -6,8 +6,17 @@ var getDieRoll = function() {
 };
 
 var firstDie = getDieRoll();
-var secondDie = getDieRoll();
+
+if(getDieRoll >= 5) {
+    console.log("Great roll!");
+    //if stays the same wherever you are... :) 
+}else if(getDieRoll >= 3) {
+    console.log("Not so gread");
+    //theese lessons are too basic
+}else {
+    console.log("Maybe you should quit gamblin`");
+    //maybe i should skip theese parts??
+}
 
 console.log(firstDie);
-console.log(secondDie);
 console.log(firstDie + secondDie);
